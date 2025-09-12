85f14daa76857b0c06580360509fec675265a5fa
MS <sunilmule045@gmail.com>
2025-09-13 00:16:48 +0530

    added router

---
+++ routes/listing.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+
+//error
+const wrapAsync = require("../utils/wrapAsync.js")
+const ExpressError = require("../utils/ExpressError.js")
+
+//model
+const Listing = require("../models/listing.js");
+
+
+// missng listing validation from server side
+
+
+
+
+
+//index route-show all aviable listings
+router.get("/",wrapAsync(async (req,res)=>{
+   const listings = await Listing.find({});
+   res.render("listings/index.ejs",{listings});
+}));
+
+//create  route 
+router.get("/new",wrapAsync(async(req,res)=>{
+    res.render("listings/new.ejs");
+}));
+
+//show route (Read)-for each individual
+router.get("/:id",wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    let obj = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs",{obj});
+}));
+
+//adding to database from new.ejs form
+router.post("/",wrapAsync(async(req,res,next)=>{
+        const lis = req.body;
+        const user = new Listing({
+        title : lis.title,
+        description : lis.description,
+        image: lis.image,
+        price : lis.price,
+        location : lis.location,
+        country : lis.country
+        });
+        if(!req.body){
+            throw new ExpressError(400,"send valid data for listing")
+        }
+        const ans = await user.save();
+        res.redirect("/listings");
+}));
+
+//edit route
+router.get("/:id/edit",wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    const obj = await Listing.findById(id);
+    res.render("listings/edit.ejs",{obj})
+}))
+//updating the database
+router.put("/:id",wrapAsync(async (req,res)=>{
+    let {id} = req.params;
+    const updatedlis = req.body;
+    if(!req.body.title){
+        throw new ExpressError(400,"send valid data for listing")
+    }
+    await Listing.findByIdAndUpdate(id,updatedlis);
+    res.redirect(`/listings/${id}`);
+}))
+//delete route
+router.delete("/:id",wrapAsync(async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+}));
+
+module.exports = router;
---
+++ routes/review.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});//for params
+
+//error
+const wrapAsync = require("../utils/wrapAsync.js")
+const ExpressError = require("../utils/ExpressError.js")
+
+//model
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");// listing model is required for post
+
+//server validation for reviews(hoppscoth)
+const {reviewSchema} = require("../schema.js")
+
+
+//function to be as middleware for review validation servers side
+const validateReview = (req,res,next)=>{
+    let {error} = reviewSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
+
+
+//adding review for individial listings
+router.post("/",validateReview,wrapAsync(async(req,res)=>{
+    let lis = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    lis.reviews.push(newReview);
+
+    await newReview.save();
+    await lis.save();
+
+    console.log("new review savd");
+    res.redirect(`/listings/${lis._id}`);
+
+}));
+
+
+//deleing a review
+
+router.delete("/:reviewId",wrapAsync(async(req,res) =>{
+    let {id,reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id,{$pull : {reviews : reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`)
+}))
+
+
+module.exports = router;
